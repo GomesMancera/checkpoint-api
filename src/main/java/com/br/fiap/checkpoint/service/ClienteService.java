e099367229da01fdf3a8a8645fc44fe38be77da1
GomesMancera <manceragomesbr@gmail.com>
2025-05-07 23:59:22 -0300

    Criação dos services de Produto e Cliente com métodos CRUD

---
+++ src/main/java/com/br/fiap/checkpoint/service/ClienteService.java
@@ -0,0 +1,32 @@
+package com.br.fiap.checkpoint.service;
+
+import com.br.fiap.checkpoint.model.Cliente;
+import com.br.fiap.checkpoint.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
