2b4091e2cc3a27275e675d26710fc23d1061b79d
GomesMancera <manceragomesbr@gmail.com>
2025-05-07 23:56:30 -0300

    Criação da estrutura de pacotes: controller, model, service e repository

---
+++ src/main/java/com/br/fiap/checkpoint/model/Produto.java
@@ -0,0 +1,31 @@
+package com.br.fiap.checkpoint.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private BigDecimal preco;
+
+    private int estoque;
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public BigDecimal getPreco() { return preco; }
+    public void setPreco(BigDecimal preco) { this.preco = preco; }
+
+    public int getEstoque() { return estoque; }
+    public void setEstoque(int estoque) { this.estoque = estoque; }
+}
